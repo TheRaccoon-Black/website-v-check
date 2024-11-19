5081dd0f1980ee0a30e3f55ba8900eae8d18b64f
muhammad fachrurrozi <ozjibuwat@gmail.com>
2024-11-20 04:04:53 +0700

    add: model resource for chekclist, petugas, and pemeriksaan

--- app/Models/Checklist.php
+++ app/Models/Checklist.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Checklist extends Model
 {
     use HasFactory;
+    protected $fillable = ['nama_item', 'kategori', 'jenis_kendaraan'];
+
+    public function scopeJenisKendaraan($query, $jenis)
+    {
+        return $query->where('jenis_kendaraan', $jenis);
+    }
 }
