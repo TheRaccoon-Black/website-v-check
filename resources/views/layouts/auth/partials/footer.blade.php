ff54ae7f197340860cb62604900bed9aaa3792a0
arief satrio <ariefsatrio098@gmail.com>
2024-12-05 11:41:23 +0700

    feat (layout): add footer

---
+++ resources/views/layouts/auth/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<section class="sm:ml-64">
+    <footer class="sticky border-t border-border bottom-0 flex shrink-0 items-center gap-2 border-b bg-background p-4">
+        <div class="w-full mx-auto max-w-screen-xl py-4 sm:py-2 sm:p-4 md:flex md:items-center md:justify-between">
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{ date('Y') }} <a
+                    href="https://flowbite.com/" class="hover:underline">{{ env('APP_NAME', 'Laravel') }}™</a>. All Rights
+                Reserved.
+            </span>
+    </footer>
+</section>
