a8835a2a0cc671b6a7c2518eceec49e887f01042
arief satrio <ariefsatrio098@gmail.com>
2024-12-22 12:19:06 +0700

    feat (logbook): add page

--- app/Http/Controllers/LoginLogController.php
+++ app/Http/Controllers/LoginLogController.php
@@ -3,15 +3,59 @@
 namespace App\Http\Controllers;
 
 use App\Models\LoginLog;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginLogController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        // Ambil semua data login logs, dengan informasi user terkait
-        $logs = LoginLog::with('user')->latest()->paginate(10);
+        $search = $request->get('search');
+        $sortBy = $request->get('sortBy');
+        $sort = $request->get('sort', 'asc');
 
-        return view('login_logs.index', compact('logs'));
+        $query = LoginLog::query();
+
+        if (Auth::user()->role == 'petugas') {
+            $query->whereHas('user', function ($query) {
+                $query->where('role', 'petugas')->where('id', Auth::user()->id);
+            });
+        }
+
+        if (!empty($search)) {
+            $query->whereHas('user', function ($query) use ($search) {
+                $query->where('name', 'like', "%{$search}%");
+            });
+        }
+
+        if (!empty($sortBy) && is_string($sortBy) && in_array($sort, ['asc', 'desc'])) {
+            if ($sortBy == 'logged_in_at') {
+                $query->orderByRaw('CAST(' . $sortBy . ' AS UNSIGNED) ' . $sort);
+            } else {
+                $query->orderBy($sortBy, $sort);
+            }
+        }
+
+        $logs = $query->paginate(10)->appends(request()->query());
+
+        $rowCallback = function ($value, $field) {
+            if ($field == 'user_id') {
+                return User::find($value)->name;
+            }
+
+            if ($field == 'logged_in_at') {
+
+                return \Carbon\Carbon::parse($value)->locale('id_ID')->translatedFormat('l, d-m-Y H:i:s');
+            }
+
+            return $value;
+        };
+
+        $sortable = (object) [
+            'logged_in_at' => (object) ['label' => 'Waktu Login', 'field' => 'logged_in_at'],
+        ];
+
+        return view('login_logs.index', compact('logs', 'sortable', 'rowCallback'));
     }
 }
